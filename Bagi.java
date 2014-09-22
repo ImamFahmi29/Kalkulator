146930f37536dc3be9528417247aad91828fe2e0
imamfahmi29 <imam.fahmi29@gmail.com>
2014-09-22 20:16:09 +0700

    kalkulator

---
+++ Bagi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Bagi {
+
+	public static void main (String [] args){
+
+	Scanner.in = Scanner (System.in)
+	
+	int x,y,z;
+
+	System.out.println("Masukkan Nilai ke-1 = " );
+
+	x = in.nextInt();
+
+	System.out.println("Masukkan Nilai ke-2 = " );
+
+	y = in.nextInt();
+
+	z = x/y;
+
+	System.out.println("Hasil = "+z );
+
+	}
+}
+
